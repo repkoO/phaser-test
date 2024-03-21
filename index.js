17ae141d59a0372c3c1a9fd2971a511d09e2238d
Kirill Medvedev <repkomail@gmail.com>
2024-03-21 22:16:18 +0200

    feat: implement animation

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const game = new Phaser.Game(600, 400, Phaser.AUTO, '', { preload: preload, crea
 
 function preload() {
 
-    this.load.image('strawberry', './assets/strawberry.jpg');
-    this.load.image('button', './assets/button.png')
+  this.load.image('strawberry', './assets/strawberry.jpg');
+  this.load.image('button', './assets/button.png')
 }
 
 let columns = [];
@@ -17,47 +17,46 @@ function create() {
   group = game.add.group()
 
   for (let i = 0; i < 5; i++) {
-      columns[i] = game.add.group();
-      for (let j = 0; j < 30; j++) {
-            const image = columns[i][j] = group.create(i * 110, j * -110, 'strawberry');
-            image.scale.set(0.15);
-            image.anchor.set(-0.3, -0.2)
-            columns[i].add(image);
-        }
+    columns[i] = game.add.group();
+    for (let j = 0; j < 40; j++) {
+      const image = columns[i][j] = group.create(i * 110, 200 + j * -100, 'strawberry');
+      image.scale.set(0.15);
+      image.anchor.set(-0.3, -0.2)
+      columns[i].add(image);
+      }
+      const mask = game.add.graphics(0, 0);
+      mask.beginFill(0xffffff);
+      mask.drawRect(i * 115, 15, 110, 300);
+      columns[i].mask = mask;
     }
 
+// Создаем кнопку
+
+  button = game.add.button(250, 350, 'button', spinSlot, this, 1, 2, 0);
+  button.scale.set(0.2)
+
+// Вращение слотов
+
+function spinSlot() {
+
+  // Высчитываем задержку для каждой колонки по индексу
+  let delay = 0;
+  columns.forEach((column) => {
+    column.tween = this.add.tween(column).to({ y: 1000 }, 750, "Linear", true, delay, 1, false);
+      // Увеличиваем задержку для каждой колонки
+      delay += 100;
+      column.tween.repeat(5)
+      column.tween.onComplete.add(function() {
+        column.tween.stop();
+      }, this);
+  });
+}
 
 
-        // Создаем кнопку
 
-        button = game.add.button(250, 350, 'button', spinSlot, this, 1, 2, 0);
-        button.scale.set(0.2)
+}
 
-        // Вращение слотов
-
-        function spinSlot() {
-
-            const mask = game.add.graphics(0, 100);
-
-            mask.drawRect(50, 0, 140, 200);
-            mask.drawRect(330, 0, 140, 200);
-            mask.drawRect(530, 0, 140, 200);
-            mask.drawRect(530, 0, 140, 200);
-            mask.drawRect(530, 0, 140, 200);
-
-            group.mask = mask;
-
-            // Применяем анимацию к каждому изображению в контейнере
-            columns.forEach(column => {
-                column.forEach(image => {
-                    // Создаем анимацию движения вверх
-                    const tweenUp = this.add.tween(image).to({ y: 1000 }, 300, "Linear", true, 0, -1);
-                    tweenUp.start();
-        });
-    });
-        }
-
-
+function update() {
 
 }
 
